49d63e3d27ac6fab4571aaec889abfc59022cccd
Matthan Cantrell <cantrellmatthan247@gmail.com>
2022-12-01 19:28:10 -0700

    big PUSHA

--- Login/MainWindow.xaml.cs
+++ Login/MainWindow.xaml.cs
@@ -34,24 +34,27 @@ namespace Login
             string sql;
             txtReturn.Text = "";
 
-            ht.Clear();
+            ht.Clear(); // Clear Hashtable before useage
 
+            // Add Values Based On User Input
             ht.Add("@Username", txtUsername.Text);
             ht.Add("@Password", txtPassword.Text);
 
+            // SQL Statement to Pull Data From Database based on user input
             sql = "Select Name from Users where Username=@Username and Password=@Password";
 
-            dt = ExDB.GetDataTable("CantrellDB", ht, sql);
+            dt = ExDB.GetDataTable("CantrellDB", ht, sql); // Grab Datatable From Database
 
             if (dt.Rows.Count > 0)
             {
-                txtReturn.Text = "Login Successful";
+                txtReturn.Text = "Login Successful"; // If The datatable returns a value matching that entered, SUCCESS
             }
             else
             {
-                txtReturn.Text = "Login Failed";
+                txtReturn.Text = "Login Failed"; // If no data in the datatable matches that entered, FAIL
             }
 
+            // Reset Values After Execution
             txtUsername.Text = "";
             txtPassword.Text = "";
         }
@@ -62,16 +65,22 @@ namespace Login
             string sql;
             long lngReturn;
 
+            // Emptying Hashtable Before Use
             ht.Clear();
 
+            // SQL Statement Made To Add/Insert User Data To Table
             sql = "Insert into Users (Name, Username, Password, Email) Values (@Name, @Username, @Password, @Email)";
+
+            // Adding Values To The Hashtable based on values provided by user
             ht.Add("@Name", txtRName.Text);
             ht.Add("@Username", txtRUsername.Text);
             ht.Add("@Password", txtRPassword.Text);
             ht.Add("@Email", txtREmail.Text);
 
+            // Execute Registration To The SQL Database
             lngReturn = ExDB.ExecuteIt("CantrellDB", sql, ht);
 
+            // Reset Values After Execution
             txtRName.Text = "";
             txtRUsername.Text = "";
             txtRPassword.Text = "";
