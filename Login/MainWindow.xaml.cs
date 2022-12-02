dc5302c086e580a6ac002cec184a07b9083ca622
Matthan Cantrell <cantrellmatthan247@gmail.com>
2022-12-01 19:10:07 -0700

    big pusha

--- Login/MainWindow.xaml.cs
+++ Login/MainWindow.xaml.cs
@@ -27,24 +27,55 @@ namespace Login
             InitializeComponent();
         }
 
-        private void btnDoIt_Click(object sender, RoutedEventArgs e)
+        private void SubmitButton_Click(object sender, RoutedEventArgs e)
         {
             DataTable dt;
+            Hashtable ht = new Hashtable();
+            string sql;
+            txtReturn.Text = "";
+
+            ht.Clear();
+
+            ht.Add("@Username", txtUsername.Text);
+            ht.Add("@Password", txtPassword.Text);
+
+            sql = "Select Name from Users where Username=@Username and Password=@Password";
+
+            dt = ExDB.GetDataTable("CantrellDB", ht, sql);
+
+            if (dt.Rows.Count > 0)
+            {
+                txtReturn.Text = "Login Successful";
+            }
+            else
+            {
+                txtReturn.Text = "Login Failed";
+            }
+
+            txtUsername.Text = "";
+            txtPassword.Text = "";
+        }
+
+        private void RegisterButton_Click(object sender, RoutedEventArgs e)
+        {
             Hashtable ht = new Hashtable();
             string sql;
             long lngReturn;
 
             ht.Clear();
 
-            sql = "Insert into Users (Name, Password) Values (@Name, @Password)";
-
-            ht.Add("@Name", txtName.Text);
-            ht.Add("@Password", txtPassword.Text);
+            sql = "Insert into Users (Name, Username, Password, Email) Values (@Name, @Username, @Password, @Email)";
+            ht.Add("@Name", txtRName.Text);
+            ht.Add("@Username", txtRUsername.Text);
+            ht.Add("@Password", txtRPassword.Text);
+            ht.Add("@Email", txtREmail.Text);
 
             lngReturn = ExDB.ExecuteIt("CantrellDB", sql, ht);
 
-            sql = "Update Names set Name=@Name, Title=@Title where ID=@ID";
-
+            txtRName.Text = "";
+            txtRUsername.Text = "";
+            txtRPassword.Text = "";
+            txtREmail.Text = "";
         }
     }
 }
